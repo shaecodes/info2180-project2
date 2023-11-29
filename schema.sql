417613e99af4568ecbc0ed9fb4203b125ae4cfef
shaecodes <sinclairroshae@yahoo.com>
2023-11-29 11:59:51 -0500

    Added schema.sql

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    role VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Contacts (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    assigned_to INTEGER NOT NULL,
+    created_by INTEGER NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL
+);
+
+CREATE TABLE Notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    contact_id INTEGER NOT NULL,
+    comment TEXT NOT NULL,
+    created_by INTEGER NOT NULL,
+    created_at DATETIME NOT NULL
+);
