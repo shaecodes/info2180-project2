c3033bc998776fa1e445d3bc18bd646c73f0f800
shaecodes <sinclairroshae@yahoo.com>
2023-12-01 17:35:12 -0500

    Edited schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE Users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     role VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -20,16 +20,19 @@ CREATE TABLE Contacts (
     telephone VARCHAR(255) NOT NULL,
     company VARCHAR(255) NOT NULL,
     type VARCHAR(255) NOT NULL,
-    assigned_to INTEGER NOT NULL,
-    created_by INTEGER NOT NULL,
+    assigned_to INT,
+    created_by INT,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL
+    updated_at DATETIME NOT NULL,
+    FOREIGN KEY (assigned_to) REFERENCES Users(id),
+    FOREIGN KEY (created_by) REFERENCES Users(id)
 );
 
 CREATE TABLE Notes (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     contact_id INTEGER NOT NULL,
     comment TEXT NOT NULL,
-    created_by INTEGER NOT NULL,
-    created_at DATETIME NOT NULL
+    created_by INT,
+    FOREIGN KEY (created_by) REFERENCES Users(id)
+    created_at DATETIME NOT NULL,
 );
