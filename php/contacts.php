84df50ebb8a6f535f7a5f00e22ed5e8db7f8296d
K-macc <mkeshawn107@gmail.com>
2023-12-06 11:21:43 -0500

    Contact.php

--- php/contacts.php
+++ php/contacts.php
@@ -1,4 +1,5 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $host = "localhost";
 $username = "proj2_user";
 $password = "groupbest1234";
@@ -16,23 +17,19 @@ $assign = $_POST['assign_user'] ?? '';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  
-    $sql = "INSERT INTO Contacts (title, firstname, lastname, email, telephone, company, type, assigned_to, created_by, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, 1,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
-    $push = $conn->prepare($sql);
-    $push->execute();
-}else{
-
 $sql = "SELECT * FROM Users";
 $result = $conn->query($sql);
 $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-
 
 foreach($rows as $row ) {
     $options .= '<option value="">'. $row['firstname']. " ". $row['lastname']. '</option>';
 }
 
-echo $options}
+echo $options;
+
+$sql1 = "INSERT INTO Contacts (title, firstname, lastname, email, telephone, company, type, assigned_to, created_by, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, 1,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
+$push = $conn->prepare($sql1);
+$push->execute();}
 
 
 ?>
