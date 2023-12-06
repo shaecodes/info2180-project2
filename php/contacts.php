6951049b9d53a282ce119c2f63964ecbbac1a28e
K-macc <mkeshawn107@gmail.com>
2023-12-06 11:19:47 -0500

    Contact.php

--- php/contacts.php
+++ php/contacts.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $username = "proj2_user";
 $password = "groupbest1234";
 $dbname = "dolphin_crm";
-$options = '<option value="">-k-</option>';
+$options = '<option value="">---</option>';
 
 $title = $_POST['title'] ?? '';
 $fname = $_POST['fname'] ?? '';
@@ -14,8 +14,14 @@ $company = $_POST['company'] ?? '';
 $type = $_POST['type'] ?? '';
 $assign = $_POST['assign_user'] ?? '';
 
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+    $sql = "INSERT INTO Contacts (title, firstname, lastname, email, telephone, company, type, assigned_to, created_by, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, 1,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
+    $push = $conn->prepare($sql);
+    $push->execute();
+}else{
 
 $sql = "SELECT * FROM Users";
 $result = $conn->query($sql);
@@ -26,11 +32,7 @@ foreach($rows as $row ) {
     $options .= '<option value="">'. $row['firstname']. " ". $row['lastname']. '</option>';
 }
 
-echo $options
+echo $options}
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $sql = "INSERT INTO Contacts (title, firstname, lastname, email, telephone, company, type, assigned_to, created_by, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, 1,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
-    $push = $conn->prepare($sql);
-    $push->execute();
-}
+
 ?>
