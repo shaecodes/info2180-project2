167879c1708043e0950449ef30dbdfe0bc0c933e
shaecodes <sinclairroshae@yahoo.com>
2023-12-06 20:15:59 -0500

    Added php session to login page

--- php/login.php
+++ php/login.php
@@ -1,4 +1,6 @@
 <?php
+session_start(); // Start the session
+
 $host = 'localhost';
 $username = 'proj2_user';
 $password = 'groupbest1234';
@@ -25,7 +27,12 @@ if ($conn->connect_error){
         
         // Check if the entered password matches the hashed password in the database
         if (password_verify($password_filter, $data['pwd'])) {
-            if ($data['_role'] === "Admin"){
+            // Set session variables
+            $_SESSION['user_id'] = $data['user_id'];
+            $_SESSION['user_email'] = $data['email'];
+            $_SESSION['user_role'] = $data['_role'];
+
+            if ($_SESSION['user_role'] === "Admin"){
                 echo "<script>alert('Login Successfully')</script>";
                 header("Refresh:1, url=../pages/users.html");
 
@@ -43,9 +50,7 @@ if ($conn->connect_error){
         header("Refresh:1, url=../pages/login.html");
     }
     
-    
     $stmt->close();
     $conn->close();
 }
-
 ?>
