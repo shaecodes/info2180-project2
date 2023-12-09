1b2de24a542f39f085746de17a9a0d677cec4a67
Jada Walters <jada.walters@mymona.uwi.edu>
2023-12-09 10:36:18 -0500

    Added CSS and edited header

--- php/header.php
+++ php/header.php
@@ -1,7 +1,14 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+</head>
 <nav>
-    <a href="../php/dashboard.php">Home</a>
-    <a href="../pages/contacts.html">New Contact</a>
-    <a href="../pages/users.html">Users</a>
-    <a href="../php/logout.php">Logout</a>
+    <img src="../images/dolphin-icon.jpeg" alt="Dolphin Icon">
+    <h1>Dolphin CRM</h1>
+    <div id="links">
+        <a href="../php/dashboard.php">Home</a>
+        <a href="../pages/contacts.html">New Contact</a>
+        <a href="../pages/users.html">Users</a>
+        <a href="../php/logout.php">Logout</a>
+    </div>
 </nav>
 
