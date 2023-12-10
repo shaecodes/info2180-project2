35878a1b545111599ae2d54ac5e3691ee9ed2294
shaecodes <sinclairroshae@yahoo.com>
2023-12-10 18:04:45 -0500

    Removed nav bar links from login

---
+++ php/login_header.php
@@ -0,0 +1,9 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+</head>
+<nav>
+    <img src="../images/dolphin-icon.jpeg" alt="Dolphin Icon">
+    <h1>Dolphin CRM</h1>
+    <div id="links"></div>
+</nav>
+
